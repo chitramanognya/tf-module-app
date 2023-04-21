b23d1fa792f7726b3e5b67d8853d90f30f4feb1c
chitra <chmanu.chinu@gmail.com>
2023-04-21 10:44:11 +0000

    update app

--- iam.tf
+++ iam.tf
@@ -8,21 +8,25 @@ resource "aws_iam_policy" "main" {
     "Version": "2012-10-17",
     "Statement": [
         {
-            "Sid": "VisualEditor0",
-            "Effect": "Allow",
-            "Action": [
+            "Sid" : "VisualEditor0",
+            "Effect" : "Allow",
+            "Action" : [
                 "ssm:GetParameterHistory",
                 "ssm:GetParametersByPath",
                 "ssm:GetParameters",
                 "ssm:GetParameter"
             ],
-            "Resource": "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}*"
+            "Resource" : [
+          
+            "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.${var.component}*"
+            "arn:aws:ssm:us-east-1:${data.aws_caller_identity.account.account_id}:parameter/${var.env}.docdb"
+            ]
         },
         {
-            "Sid": "VisualEditor1",
-            "Effect": "Allow",
-            "Action": "ssm:DescribeParameters",
-            "Resource": "*"
+            "Sid" : "VisualEditor1",
+            "Effect" : "Allow",
+            "Action" : "ssm:DescribeParameters",
+            "Resource" : "*"
         }
     ]
 })
